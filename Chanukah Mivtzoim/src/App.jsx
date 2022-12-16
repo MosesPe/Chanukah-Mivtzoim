9c7df95c3cc36ed2c0972e6ecefafe5fda43f441
MosesPe <ipekar@gmail.com>
2022-12-16 11:13:05 -0500

    Added header and basic Menorah

---
+++ Chanukah Mivtzoim/src/App.jsx
@@ -0,0 +1,30 @@
+
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+  <h1 className="header">Happy Chanukah</h1>
+  <div className="Menorah">
+    <div className="candles Container">
+      <span className="candle"></span>
+      <span className="candle"></span>
+      <span className="candle"></span>
+      <span className="candle"></span>
+      <span className="candle shamash"></span>
+      <span className="candle"></span>
+      <span className="candle"></span>
+      <span className="candle"></span>
+      <span className="candle"></span>
+    </div><br />
+    <div className="Container"><div className="base"></div></div>
+
+  </div>
+    </div>
+  )
+}
+
+export default App
